9965d5ba84bfe4c7dc4084ec88c059a6d127351f
isokar <matthieu_lemaitre@yahoo.com>
2018-09-27 20:43:38 +0200

    Update system.get.js

--- controller/system.get.js
+++ controller/system.get.js
@@ -1,19 +1,4 @@
-const shared = require('../lib/shared.js');
 
 module.exports = (req, res, next) => {
-    shared
-        .player
-        .authorizationCodeGrant(req.query.code)
-        .then(({body: {access_token, refresh_token}}) => {
-            
-            shared.player.setAccessToken(access_token);
-            shared.player.setRefreshToken(refresh_token);
-            
-            gladys.param.setValue({name: 'spotify_refresh_token', value: refresh_token});
-
-            res.status(200).json({message: 'Token successful added'});
-        })
-        .catch(error => {
-            res.status(400).json(error);
-        })
+    res.status(200).json({"id": "Gladys","apiversion": 1});
 }
