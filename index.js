3d26ba9ddec33db4eec501f474291cabb70bc328
isokar <matthieu_lemaitre@yahoo.com>
2018-09-27 20:34:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 
-module.exports = function(sails){
-	var emit = require('./lib/emit.js');
-	var connect = require('./lib/connect.js');
-	
-	gladys.on('ready', function(){
-		connect();
-	});
-	
-	return {
-		emit: emit,
-	};
+var setup = require('./lib/setup');
+var init = require('./lib/init');
+var shared = require('./lib/shared');
+var systemcontroller = require('./controller/system.get');
 
+module.exports = function (sails) {
+    
+    gladys.on('ready', init)
+
+    return {
+        setup,
+        init,
+        shared,
+        routes: {
+            after: {
+                'GET /system': systemcontroller
+            }
+        }
+    };
 };
